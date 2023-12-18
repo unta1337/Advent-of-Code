8a4fa9936678bc0deff0b31ee3a71d0479ab8cbd
unta1337 <firstapple42@gmail.com>
2023-12-18 23:38:29 +0900

    Day 18: Part 2 추가

--- 2023/day18/main.c
+++ 2023/day18/main.c
@@ -4,6 +4,10 @@
 #include <string.h>
 #include <errno.h>
 
+#define ABS(x) ((x) > (x) ? (x) : -(x))
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
 #define LEN_LINES 700
 #define LEN_LINE 50
 
@@ -11,23 +15,23 @@
 
 typedef struct {
     char* str;
-    size_t len;
+    long long len;
 } string_t;
 
 typedef enum {
-    LEFT,
+    RIGHT,
     DOWN,
-    UP,
-    RIGHT
+    LEFT,
+    UP
 } dir_t;
 
-int dirs[][2] = {
+long long dirs[][2] = {
     [LEFT] = { 0, -1 },
     [DOWN] = { 1, 0 },
     [UP] = { -1, 0 },
     [RIGHT] = { 0, 1 }
 };
-size_t dir_count = sizeof(dirs) / sizeof(dirs[0]);
+long long dir_count = sizeof(dirs) / sizeof(dirs[0]);
 
 dir_t get_dir(char d) {
     switch (d) {
@@ -40,15 +44,20 @@ dir_t get_dir(char d) {
     return LEFT;
 }
 
-bool is_inside(char grid[GRID_LEN][GRID_LEN], size_t row, size_t col) {
-    size_t r = row;
-    size_t c = col;
+typedef struct {
+    long long row;
+    long long col;
+} pos_t;
+
+bool is_inside(char grid[GRID_LEN][GRID_LEN], long long row, long long col) {
+    long long r = row;
+    long long c = col;
 
     if (grid[r][c] == '#') {
         return false;
     }
 
-    size_t count = 0;
+    long long count = 0;
     while (c < GRID_LEN) {
         if (grid[r][c] == '#') {
             dir_t begin_dir = DOWN;
@@ -59,7 +68,7 @@ bool is_inside(char grid[GRID_LEN][GRID_LEN], size_t row, size_t col) {
             } else {
                 begin_dir = grid[r + 1][c] == '#' ? DOWN : UP;
             }
-            size_t temp = 0;
+            long long temp = 0;
 
             while (c < GRID_LEN && grid[r][c] == '#') {
                 c--;
@@ -88,38 +97,38 @@ bool is_inside(char grid[GRID_LEN][GRID_LEN], size_t row, size_t col) {
     return count % 2 == 0 ? false : true;
 }
 
-size_t part_one(string_t* lines, size_t len_lines) {
+long long part_one(string_t* lines, long long len_lines) {
     char grid[GRID_LEN][GRID_LEN] = { 0, };
 
-    size_t r = GRID_LEN / 2;
-    size_t c = GRID_LEN / 2;
+    long long r = GRID_LEN / 2;
+    long long c = GRID_LEN / 2;
     grid[r][c] = '#';
 
-    for (size_t i = 0; i < len_lines; i++) {
+    for (long long i = 0; i < len_lines; i++) {
         char* dir_str = strtok(lines[i].str, " ");
         dir_t dir = get_dir(*dir_str);
 
         char* count_str = strtok(NULL, " ");
-        size_t count = strtoll(count_str, NULL, 10);
+        long long count = strtoll(count_str, NULL, 10);
 
-        for (size_t j = 0; j < count; j++) {
+        for (long long j = 0; j < count; j++) {
             r += dirs[dir][0];
             c += dirs[dir][1];
             grid[r][c] = '#';
         }
     }
 
-    for (size_t i = 0; i < GRID_LEN; i++) {
-        for (size_t j = 0; j < GRID_LEN; j++) {
+    for (long long i = 0; i < GRID_LEN; i++) {
+        for (long long j = 0; j < GRID_LEN; j++) {
             if (is_inside(grid, i, j)) {
                 grid[i][j] = '@';
             }
         }
     }
 
-    size_t answer = 0;
-    for (size_t i = 0; i < GRID_LEN; i++) {
-        for (size_t j = 0; j < GRID_LEN; j++) {
+    long long answer = 0;
+    for (long long i = 0; i < GRID_LEN; i++) {
+        for (long long j = 0; j < GRID_LEN; j++) {
             answer += grid[i][j] == '#' || grid[i][j] == '@';
         }
     }
@@ -127,12 +136,58 @@ size_t part_one(string_t* lines, size_t len_lines) {
     return answer;
 }
 
-size_t part_two(string_t* lines, size_t len_lines) {
-    for (size_t i = 0; i < len_lines; i++) {
-        printf("%4zu: \"%s\" (%zu)\n", i, lines[i].str, lines[i].len);
+long long part_two(string_t* lines, long long len_lines) {
+    pos_t points[2 * LEN_LINES] = { 0, };
+    long long point_count = 0;
+
+    long long r = GRID_LEN / 2;
+    long long c = GRID_LEN / 2;
+
+    for (long long i = 0; i < len_lines; i++) {
+        strtok(lines[i].str, "#");
+        char* token = strtok(NULL, ")");
+
+        long long dir = token[5] - '0';
+        token[5] = '\0';
+
+        long long count = strtoll(token, NULL, 16);
+
+        points[point_count].row = r;
+        points[point_count].col = c;
+        point_count++;
+
+        r += count * dirs[dir][0];
+        c += count * dirs[dir][1];
     }
 
-    return 0;
+    long long border_count = 0;
+    long long area = 0;
+
+    for (long long i = 0; i < point_count; i++) {
+        pos_t* curr = &points[i];
+        pos_t* next = &points[(i + 1) % point_count];
+
+        long long begin = 0;
+        long long end = 0;
+
+        if (curr->row == next->row) {
+            begin = MIN(curr->col, next->col);
+            end = MAX(curr->col, next->col);
+        } else {
+            begin = MIN(curr->row, next->row);
+            end = MAX(curr->row, next->row);
+        }
+
+        border_count += end - begin;
+
+        area += curr->row * next->col - curr->col * next->row;
+    }
+    area = ABS(area) / 2;
+
+    long long inner = area - border_count / 2 + 1;
+    long long answer = inner + border_count;
+
+    return answer;
 }
 
 void solve(const char* input_path) {
@@ -144,10 +199,10 @@ void solve(const char* input_path) {
     }
 
     string_t lines[LEN_LINES] = { 0, };
-    for (size_t i = 0; i < LEN_LINES; i++) {
+    for (long long i = 0; i < LEN_LINES; i++) {
         lines[i].str = (char*)calloc(LEN_LINE, sizeof(char));
     }
-    size_t line_count = 0;
+    long long line_count = 0;
 
     // load input file into buffer line-by-line.
     for (line_count = 0; fgets(lines[line_count].str, LEN_LINE, fp) != NULL; line_count++) {
@@ -158,9 +213,9 @@ void solve(const char* input_path) {
 
     // print answer.
 #ifndef PART_TWO
-    printf("%zu\n", part_one(lines, line_count));
+    printf("%lld\n", part_one(lines, line_count));
 #else
-    printf("%zu\n", part_two(lines, line_count));
+    printf("%lld\n", part_two(lines, line_count));
 #endif
 
     fclose(fp);
