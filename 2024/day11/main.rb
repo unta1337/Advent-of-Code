f6684fdccc85d0497e20daa97861d62c670739b6
unta1337 <firstapple42@gmail.com>
2024-12-11 22:58:06 +0900

    Part 2 메모이제이션 최적화 적용

--- 2024/day11/main.rb
+++ 2024/day11/main.rb
@@ -23,22 +23,25 @@ puts part1
 puts
 
 # Part 2
-part2 = input.clone
-75.times do |i|
-  puts "Part 2: #{i + 1}/#{75}"
-
-  part2.map! { |stone|
-    if stone == 0
-      [1]
-    elsif stone.to_s.size % 2 == 0
-      s = stone.to_s
-      [s[...s.size / 2].to_i, s[s.size / 2...].to_i]
-    else
-      [stone * 2024]
-    end
-  }.flatten!
+def calc_part2 i, depth, cache = Hash.new
+  if cache.include?([i, depth])
+    cache[[i, depth]]
+  else
+    cache[[i, depth]] = if depth == 0
+                 1
+               elsif i == 0
+                 calc_part2 1, depth - 1, cache
+               elsif i.to_s.size % 2 == 0
+                 s = i.to_s
+                 calc_part2(s[...s.size / 2].to_i, depth - 1, cache) +
+                   calc_part2(s[s.size / 2...].to_i, depth - 1, cache)
+               else
+                 calc_part2 i * 2024, depth - 1, cache
+               end
+  end
 end
-part2 = part2.size
+
+part2 = input.map.with_index { |i, j| calc_part2 i, 75 }.sum
 
 puts "== Part 2 =="
 puts part2
