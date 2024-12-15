ea505b5e49a18540e1ab7eab33a8109cabd84b38
unta1337 <firstapple42@gmail.com>
2024-12-15 20:14:55 +0900

    Part 2 완

--- 2024/day15/main.rb
+++ 2024/day15/main.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-$sample = true
+$sample = false
 input_path = $sample ? "sample.txt" : "input.txt"
 
 input = File.open(input_path, "r").map(&:strip)
@@ -43,6 +43,69 @@ def move_box(field, row, col, dir)
   [row + dr, col + dc]
 end
 
+def get_push_loc(field, row, col, dir, visited = nil)
+  if visited.nil?
+    visited = Array.new(field.size) { Array.new(field[0].size, false) }
+  end
+
+  visited[row][col] = true
+
+  ret = [[row, col]]
+  ddr, ddc = $dirs[dir]
+
+  [[-1, 0], [1, 0], [0, -1], [0, 1]].each do |dr, dc|
+    if [ddr, ddc] == [-dr, -dc] || ddc != 0 && dr != 0
+      next
+    end
+
+    nr, nc = row + dr, col + dc
+    if visited[nr][nc]
+      next
+    end
+
+    if ddc != 0
+      if field[nr][nc] == "#"
+        ret = []
+      elsif dc != 0 && "[]".include?(field[nr][nc])
+        res = get_push_loc(field, nr, nc, dir, visited)
+        ret = res.size > 0 && ret.size > 0 ? ret + res : []
+      end
+    else
+      if dr != 0
+        if "[]".include?(field[nr][nc])
+          res = get_push_loc(field, nr, nc, dir, visited)
+          ret = res.size > 0 && ret.size > 0 ? ret + res : []
+        elsif field[nr][nc] != "."
+          ret = []
+        end
+      else
+        pair = dc < 0 ? field[nr][nc] + field[row][col] : field[row][col] + field[nr][nc]
+        if pair == "[]"
+          res = get_push_loc(field, nr, nc, dir, visited)
+          ret = res.size > 0 && ret.size > 0 ? ret + res : []
+        end
+      end
+    end
+  end
+
+  ret
+end
+
+def push_box(field, locs, dir)
+  bak = Hash.new
+  locs.each do |r, c|
+    bak[[r, c]] = field[r][c]
+    field[r][c] = "."
+  end
+
+  dr, dc = $dirs[dir]
+  locs.each do |r, c|
+    field[r + dr][c + dc] = bak[[r, c]]
+  end
+
+  [locs[0][0] + dr, locs[0][1] + dc]
+end
+
 # Part 1
 f = field.deep_clone
 pos = (0...f.size).to_a.product((0...f[0].size).to_a).find { |r, c| f[r][c] == "@" }
@@ -66,10 +129,16 @@ puts
 f = field.map { |row| row.chars.map { |cell| cell == "O" ? "[]" : cell == "@" ? "@." : cell * 2 }.join }
 pos = (0...f.size).to_a.product((0...f[0].size).to_a).find { |r, c| f[r][c] == "@" }
 
-puts f.join("\n")
-puts "#{pos}"
+moves.each do |move|
+  locs = get_push_loc(f, *pos, move)
+  if locs.size > 0
+    pos = push_box(f, locs, move)
+  end
+end
 
-part2 = 0
+part2 = (0...f.size).to_a.product((0...f[0].size).to_a)
+  .map { |r, c| f[r][c] == "[" ? r * 100 + c : 0 }
+  .sum
 
 puts "== Part 2 =="
 puts part2
