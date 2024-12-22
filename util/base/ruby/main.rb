c2939f1eb1cbde138ce2c69edf1b5c86e0a12da8
unta1337 <firstapple42@gmail.com>
2024-12-22 18:11:24 +0900

    루비 기본 템플릿 추가

---
+++ util/base/ruby/main.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+$sample = true
+input_path = $sample ? "sample.txt" : "input.txt"
+
+input = File.open(input_path, "r").map(&:strip)
+
+# Part 1
+part1 = 0
+
+puts "== Part 1 =="
+puts part1
+puts
+
+# Part 2
+part2 = 0
+
+puts "== Part 2 =="
+puts part2
